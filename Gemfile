3dc9cef569a3138e12ca62d07eff26464475d676
Marcel Hild <hild@b4mad.net>
2017-05-15 14:47:52 +0200

    s2i

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-gem 'rack'
+gem 'sinatra'
 gem 'puma'
 gem 'ruby-kafka'
 
--- client.rb
+++ client.rb
@@ -1,12 +1,36 @@
 require "kafka"
-
+logger = Logger.new(STDOUT)
 kafka = Kafka.new(
   # At least one of these nodes must be available:
-  seed_brokers: ["192.168.64.2:30528"],
-
+  seed_brokers: ["192.168.64.2:31956"],
+  logger: logger,
   # Set an optional client id in order to identify the client to Kafka:
-  client_id: "my-application",
+  # client_id: "my-application",
 )
 
-kafka.deliver_message("Hello, World!", topic: "greetings")
+# `#async_producer` will create a new asynchronous producer.
+producer = kafka.async_producer
+
+# The `#produce` API works as normal.
+producer.produce("hello", topic: "greetings")
+
+# `#deliver_messages` will return immediately.
+producer.deliver_messages
+
+# Make sure to call `#shutdown` on the producer in order to avoid leaking
+# resources. `#shutdown` will wait for any pending messages to be delivered
+# before returning.
+producer.shutdown
+
+# producer = kafka.producer
+# Add a message to the producer buffer.
+# producer.produce("hello1", topic: "test-messages")
+
+# Deliver the messages to Kafka.
+# producer.deliver_messages
+
+# kafka.each_message(topic: "funky") do |message|
+#   puts message.offset, message.key, message.value
+# end
+# kafka.deliver_message("Hello, World!", topic: "funky")
 
